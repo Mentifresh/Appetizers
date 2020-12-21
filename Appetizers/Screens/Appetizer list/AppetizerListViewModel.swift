7af399573ae77902621bf4beddd31b8fb33f9081
Daniel Kilders <dankil.dev@gmail.com>
2020-12-21 17:50:22 +0100

    Added error handling

--- Appetizers/Screens/Appetizer list/AppetizerListViewModel.swift
+++ Appetizers/Screens/Appetizer list/AppetizerListViewModel.swift
@@ -3,15 +3,25 @@ import Foundation
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
-        NetworkManager.shared.getAppetizers { (result) in
+        NetworkManager.shared.getAppetizers { [self] (result) in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print("Error when getting errors", error.localizedDescription)
+                    switch error {
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
