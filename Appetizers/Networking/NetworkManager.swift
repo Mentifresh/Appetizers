804eb71eb7008bc8f8b5e9cdd65e0d28dc867935
Daniel Kilders <dankil.dev@gmail.com>
2020-12-23 14:22:13 +0100

    Added network call to fetch images

--- Appetizers/Networking/NetworkManager.swift
+++ Appetizers/Networking/NetworkManager.swift
@@ -1,9 +1,9 @@
-import Foundation
-
+import UIKit
 
 final class NetworkManager {
     
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers/"
@@ -42,6 +42,38 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        
+        task.resume()
+    }
+    
+    func getProfilePicture(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void ) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let _ = error,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200,
+                let data = data,
+                let image = UIImage(data: data)
+            else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         
         task.resume()
